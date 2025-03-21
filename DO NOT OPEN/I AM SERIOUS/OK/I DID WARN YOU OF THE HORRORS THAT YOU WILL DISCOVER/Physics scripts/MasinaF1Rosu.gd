e3ebffad3816f7c68b472ed8b6696e02372ba8d8
avg517 <dragosjocuri@gmail.com>
2025-03-21 14:32:04 +0200

    V1.03.001

--- DO NOT OPEN/I AM SERIOUS/OK/I DID WARN YOU OF THE HORRORS THAT YOU WILL DISCOVER/Physics scripts/MasinaF1Rosu.gd
+++ DO NOT OPEN/I AM SERIOUS/OK/I DID WARN YOU OF THE HORRORS THAT YOU WILL DISCOVER/Physics scripts/MasinaF1Rosu.gd
@@ -19,20 +19,20 @@ func friction (force):
 func breaks(force):
 	if(speed.x*cos (global_rotation) > 0 and speed.y *sin (global_rotation)>0):
 		speed -= Vector2(cos (global_rotation) * force,sin (global_rotation) * force)#*(speed.length()/2)  *(speed.length()/2)
-	
+		#friction(30)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	#movement 
 	var LabelP1 = "%s KM/H"
-	get_node("Label").text = LabelP1 % int(speed.length()/2)
+	get_node("Label").text = LabelP1 % int(speed.length()/4)
 	if Input.is_action_pressed("P1_Move_Up"):
 		speed += Vector2(cos (global_rotation) * acc,sin (global_rotation) * acc)
 	if Input.is_action_pressed("P1_Move_Down"):
 		breaks(10.0)
 		#friction(20.0);
-	friction(1);
+	friction(2);
 		
 	velocity = speed
 	move_and_slide()
@@ -42,6 +42,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("P1_Move_Left"):
 		global_rotation_degrees -= 5
 	var overlapping_mobs = %coll.get_overlapping_bodies()
-	if overlapping_mobs.size() > 1:
-		speed = Vector2(0,0)
+	if (overlapping_mobs.size() > 1 and speed.x*cos (global_rotation) > 0 and speed.y *sin (global_rotation)>0):
+		speed -= Vector2(cos (global_rotation) * 100.0,sin (global_rotation) * 100.0)
 	#menuEntering

f2b6fdcb525add525e12b8358d2391c821be2915
avg517 <dragosjocuri@gmail.com>
2025-03-31 01:12:31 +0300

    gearbox 1600

--- DO NOT OPEN/I AM SERIOUS/OK/I DID WARN YOU OF THE HORRORS THAT YOU WILL DISCOVER/Physics scripts/MasinaF1Rosu.gd
+++ DO NOT OPEN/I AM SERIOUS/OK/I DID WARN YOU OF THE HORRORS THAT YOU WILL DISCOVER/Physics scripts/MasinaF1Rosu.gd
@@ -4,7 +4,10 @@ var fric = 500.0
 #var acc=5.0
 #velocity idk
 #gears top speed
-var gears =[20.0,34.0,51.0,68.0,85.0,102.0,120.0]
+var gears2 =[20.0,62.8,78.5,100.0,122.0,157.5,220.0]
+var torq2 = [20.0,10.0,5.6,4.4,3.6,2.8,2.0]
+
+var gears1 =[20.0,34.0,51.0,68.0,85.0,102.0,120.0]
 var gear=1
 var gear1=34.0
 var gear2=51.0
@@ -48,34 +51,37 @@ func accel(acceler,directie):
 		speed -= Vector2(cos (global_rotation) * acceler,sin (global_rotation) * acceler)
 func automatic():
 	
-	if(int(speed.length()/4)>=gears[gear] and gear!=6):
+	if(int(speed.length()/4)>=gears1[gear] and gear!=6):
 		gear+=1
-	if(int(speed.length()/4)<=gears[gear-1] and gear!=1):
+	if(int(speed.length()/4)<=gears1[gear-1] and gear!=1):
 		gear-=1
 
 func _physics_process(delta):
-	
-	if(gear==1):
-		topSpeed=gear1
-		torq = 17.5
-	elif(gear==2):
-		topSpeed=gear2
-		torq = 12.0
-	elif(gear==3):
-		topSpeed=gear3
-		torq = 7.0
-	elif(gear==4):
-		topSpeed=gear4
-		torq = 5.0
-	elif(gear==5):
-		topSpeed=gear5
-		torq = 3.0
-	elif(gear==6):
-		topSpeed=gear6
-		torq = 2.0
-	elif(gear==0):
-		topSpeed=20.0
-		torq=20.0
+	if (Global.top_speed==120):
+		if(gear==1):
+			topSpeed=gear1
+			torq = 17.5
+		elif(gear==2):
+			topSpeed=gear2
+			torq = 12.0
+		elif(gear==3):
+			topSpeed=gear3
+			torq = 7.0
+		elif(gear==4):
+			topSpeed=gear4
+			torq = 5.0
+		elif(gear==5):
+			topSpeed=gear5
+			torq = 3.0
+		elif(gear==6):
+			topSpeed=gear6
+			torq = 2.0
+		elif(gear==0):
+			topSpeed=20.0
+			torq=20.0
+	elif(Global.top_speed==220):
+		topSpeed=gears2[gear]
+		torq = torq2[gear]
 	#movement 
 	Global.poz1Y = global_position.y
 	Global.poz1X = global_position.x
@@ -98,9 +104,9 @@ func _physics_process(delta):
 		else:
 			accel(torq,2)
 	if(Global.manual==false):
-		if(int(speed.length()/4)>=gears[gear] and gear!=6):
+		if(int(speed.length()/4)>=gears1[gear] and gear!=6):
 			gear+=1
-		if(int(speed.length()/4)<=gears[gear-1] and gear!=1):
+		if(int(speed.length()/4)<=gears1[gear-1] and gear!=1):
 			gear-=1
 		#automatic()
 	if Input.is_action_pressed("P1_Move_Down"):

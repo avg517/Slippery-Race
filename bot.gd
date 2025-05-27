aaff61f2ad40692d1ac154b0116b37481fba96be
avg517 <dragosjocuri@gmail.com>
2025-05-27 22:26:38 +0300

    Update bot.gd
    Added more car mechanics

--- bot.gd
+++ bot.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 #speed += Vector2(cos (global_rotation) * acceler,sin (global_rotation) * acceler)   miscarea 
 var redy = false
 var touch = $PathDetect.get_overlapping_bodies()
+var speed
+
+func accel(acceler):
+	speed += Vector2(cos (global_rotation) * acceler,sin (global_rotation) * acceler)
 func _physics_process(delta: float) -> void:
 	if(redy==true):
 		if(touch>=1):
 			Global.bot_touch=true
 		else:
 			Global.bot_touch=false
+		velocity = speed
 		

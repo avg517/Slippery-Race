43e2572a9b4d8d520a59d20bb6dd42ebde7d422d
avg <dragosjocuri@gmail.com>
2025-05-29 13:11:14 +0300

    fixed camera settings, screwed gearbox settings

--- camera_settings.gd
+++ camera_settings.gd
@@ -8,7 +8,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	if(Global.smooth_camera== true):
+		$Automatic/smooth_camera.set_texture("res://Draws/Smooth_camera_on.png")
+	else:
+		$Automatic/smooth_camera.set_texture("res://Draws/Smooth_camera_off.png")
+
 
 func _on_automatic_pressed() -> void:
-	
+	if(Global.smooth_camera== false):
+		Global.smooth_camera= true
+	else:
+		Global.smooth_camera= false
